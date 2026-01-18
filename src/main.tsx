d39ece15e34383e789768e0bf20aea25858af1cf
Nabaskar Brahma <nabaskarforcode99@gmail.com>
2026-01-18 21:44:25 +0530

    fix: restore main app (smoke test confirmed infra issue)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
-import SmokeTest from './SmokeTest.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SmokeTest />
-    {/* <App /> */}
+    <App />
   </StrictMode>,
 )
